c4108b79f5a2bd9df342ff1935a597168ba8675c
TheBloodMage <nachiket663@gmail.com>
2016-11-13 00:56:12 -0800

    RabbitMQ_AIRBNB-BackEndServer
    RabbitMQ_AIRBNB-BackEndServer Added

---
+++ RabbitMQ_AIRBNB-BackEndServer/server.js
@@ -0,0 +1,57 @@
+//super simple rpc server example
+var amqp	= require('amqp');
+var util	= require('util');
+var mongo	= require("./services/mongo");
+var airbnb_services = require('./services/airbnb_services');
+
+var connection = amqp.createConnection({
+	host : '127.0.0.1'
+});
+
+connection.on('ready', function() {
+	console.log("LISTENING ON QUEUES");
+	
+	connection.queue('register_new_user_queue', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_register_new_user_queue_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+	
+	connection.queue('logout_queue', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_logout_queue_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+});
---
+++ RabbitMQ_AIRBNB-BackEndServer/services/airbnb_services.js
@@ -0,0 +1,82 @@
+var ejs 		= require("ejs");
+var mongo 		= require("./mongo");
+var ObjectId 	= require('mongodb').ObjectID;
+var mongoURL 	= "mongodb://localhost:27017/AirbnbDatabaseMongoDB";
+
+exports.handle_register_new_user_queue_request = function (msg, callback) {
+
+	var json_responses = {};
+	console.log("IN handle_register_new_user_queue_request:");
+
+	var saltRounds 			= msg.saltRounds;
+	var myPlaintextPassword = msg.myPlaintextPassword;
+	var salt 				= msg.salt;
+	var hash 				= msg.hash;
+	var dt					= msg.dt;
+	var first_name 			= msg.first_name;
+	var last_name 			= msg.last_name;
+	var inputUsername 		= msg.inputUsername;
+	var inputPassword 		= msg.inputPassword;
+
+	console.log("LISTENING TO handle_register_new_user_queue_request WITH msg_payload AS: ");
+	console.log(msg);
+	
+	mongo.connect(mongoURL, function() {
+		console.log('CONNECTED TO MONGO IN handle_register_new_user_queue_request');
+		
+		var collection_login 	= mongo.collection('login');
+		var json_responses;
+
+		collection_login.findOne({
+			username : inputUsername
+		}, function(err, user) {
+			if (user) {
+				console.log("USER ALREADY EXISTS");
+				
+				json_responses = {
+					"statusCode" : 402
+				};
+				callback(null, json_responses);
+			} else {
+				collection_login.insert({
+					user_id_ssn_format : "",
+					fname 	: first_name,
+					lname 	: last_name,
+					address : "",
+					city	: "",
+					state	: "",
+					zip_code: "",
+					phone_number	: "",
+					rating	: "",
+					reviews	: "",
+					profile_image	: "",
+					credit_card_details	: "",
+					currentlogintime: dt,
+					logintime 		: dt,
+					username : inputUsername,
+					password : hash
+				}, function(err, user) {
+					if (user) {
+						
+						json_responses = {
+							"statusCode" : 200
+						};
+						callback(null, json_responses);
+
+					} else {
+						console.log("RETURNED FALSE");
+						
+						json_responses = {
+							"statusCode" : 401
+						};
+						callback(null, json_responses);
+					}
+				});
+
+			}
+
+		});
+
+	});
+
+};
