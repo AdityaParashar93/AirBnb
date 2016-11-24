eaa88474174617d58545f6f7f738bf33cadf7f78
jnirgudkar <jayajit.nirgudkar@sjsu.edu>
2016-11-24 00:45:44 -0800

    profile, account and edit profile

--- AirBnb_CMPE273_Team10/routes/profile.js
+++ AirBnb_CMPE273_Team10/routes/profile.js
@@ -12,6 +12,10 @@ var logDir		= 'log';
 var env 		= process.env.NODE_ENV || 'development';
 var mongoURL 	= "mongodb://localhost:27017/AirbnbDatabaseMongoDB";
 
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+
 
 exports.land = function(req, res) {
 	console.log("********************************here ********************************");
@@ -28,3 +32,116 @@ exports.land = function(req, res) {
         }
     });
 };
+
+
+exports.view_profile = function(req,res)
+{
+    /* console.log("IN view_profile FUNCTION");
+     // img path
+     var imgPath = "C:\\Users\\jnirg\\Desktop\\PIXECT-20161114145523.jpg";
+
+     // connect to mongo
+     mongoose.connect('localhost', 'testing_storeImg');
+
+     // example schema
+     var schema = new Schema({
+     img: { data: Buffer, contentType: String }
+     });
+
+     // our model
+     var A = mongoose.model('A', schema);
+
+     mongoose.connection.on('open', function () {
+     console.error('mongo is open');
+
+     // empty the collection
+     A.remove(function (err) {
+     if (err) throw err;
+
+     console.error('removed old docs');
+
+     // store an img in binary in mongo
+     var a = new A;
+     a.img.data = fs.readFileSync(imgPath);
+     a.img.contentType = 'image/png';
+     a.save(function (err, a) {
+     if (err) throw err;
+
+     console.error('saved img to mongo');
+
+     // start a demo server
+     var server = express.createServer();
+     server.get('/', function (req, res, next) {
+     A.findById(a, function (err, doc) {
+     if (err) return next(err);
+     res.contentType(doc.img.contentType);
+     res.send(doc.img.data);
+     });
+     });
+
+     server.on('close', function () {
+     console.error('dropping db');
+     mongoose.connection.db.dropDatabase(function () {
+     console.error('closing db connection');
+     mongoose.connection.close();
+     });
+     });
+
+     process.on('SIGINT', function () {
+     server.close();
+     });
+
+     server.listen(3333, function (err) {
+     console.error('press CTRL+C to exit');
+     });
+     });
+     });
+     });*/
+    console.log("IN VIEW PROFILE FUNCTION");
+    MongoClient.connect(mongoURL, function (err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            //HURRAY!! We are connected. :)
+            console.log('Connection established to', mongoURL);
+
+            console.log(req.session.username);
+            var collection = db.collection('login');
+            collection.find({username: req.session.username}).toArray(function(err,result){
+                if(err)
+                {
+                    //console.log(result);
+                    console.log("ERRR------------"+err);
+                    //throw err;
+                    db.close();
+                    return done(err);
+                }
+                if(!result) {
+                    console.log(result);
+                    console.log("REsult not found!!!!!!!!-----");
+                    return done(null, false);
+                }
+
+                else if(result)
+                {
+                    console.log(result);
+                    console.log("Found it  bitch!!!!!!!!-----");
+                    //console.log("found user." + result[0]);
+                    db.close();
+                    //req.session.username = username;1
+                    response={"statusCode" : 200, "Result"	:	result};
+                    res.send(response);
+                    //done(null, result);
+                }
+            });
+        }
+    });
+
+}
+
+
+
+exports.change_password = function(req, res)
+{
+    console.log("IN CHANGE PASSWORD");
+}
