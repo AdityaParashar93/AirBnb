b8d3e88e6e9114018750ba7f569eae64894680f8
jnirgudkar <jayajit.nirgudkar@sjsu.edu>
2016-11-24 00:44:57 -0800

    profile, account and edit profile angular

--- AirBnb_CMPE273_Team10/public/angularjs/profile.js
+++ AirBnb_CMPE273_Team10/public/angularjs/profile.js
@@ -19,5 +19,39 @@ profile.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 profile.controller('userprofile', function($scope, $http,$state) {
 
+    $scope.Init = function()
+    {
+
+        $http({
+            method : "POST",
+            url : '/view_profile',
+        }).success(function(data) {
+
+            if (data.statusCode === 200) {
+                console.log("Success");
+                $scope.fname = data.Result[0].fname;
+                $scope.lname = data.Result[0].lname;
+                $scope.username = data.Result[0].username;
+
+
+            } else {
+                console.log("Failure");
+            }
+
+        });
+    }
+
+
+
+
+    $scope.Account = function()
+    {
+        window.location.assign("/Account");
+    }
+
+    $scope.EditProfile = function()
+    {
+        window.location.assign("/EditProfile");
+    }
 
 });
