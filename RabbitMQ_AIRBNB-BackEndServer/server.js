0ab2bc4a9a64292934942295be30bbd59cb4bdf5
TheBloodMage <nachiket663@gmail.com>
2016-11-26 02:38:27 -0800

    modifing the rabbitMQ server
    Adding Queue for,
    1. top ten hosts
    2. citywise revenue
    3. search host areawise

--- RabbitMQ_AIRBNB-BackEndServer/server.js
+++ RabbitMQ_AIRBNB-BackEndServer/server.js
@@ -55,6 +55,94 @@ connection.on('ready', function() {
 		});
 	});
 	
+	connection.queue('admin_list_city_names', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_admin_list_city_names_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+	
+	connection.queue('admin_list_city_hosts', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_admin_list_city_hosts_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+	
+	connection.queue('admin_citywise_revenue', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_admin_citywise_revenue_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+	
+	connection.queue('admin_top_ten_properties', function(q) {
+
+		q.subscribe(function(message, headers, deliveryInfo, m) {
+
+			util.log(util.format(deliveryInfo.routingKey, message));
+			util.log("Message: " + JSON.stringify(message));
+			util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+
+			airbnb_services.handle_admin_top_ten_properties_request(message, function(err,
+					res) {
+
+				// return index sent
+				connection.publish(m.replyTo, res, {
+					contentType : 'application/json',
+					contentEncoding : 'utf-8',
+					correlationId : m.correlationId
+				});
+
+			});
+		});
+	});
+	
 	
 	connection.queue('admin_approve_user_queue', function(q) {
 
--- RabbitMQ_AIRBNB-BackEndServer/services/airbnb_services.js
+++ RabbitMQ_AIRBNB-BackEndServer/services/airbnb_services.js
@@ -117,6 +117,81 @@ exports.handle_admin_list_user_request = function (msg, callback) {
 
 };
 
+
+exports.handle_admin_list_city_names_request = function (msg, callback) {
+
+	console.log("IN handle_admin_list_city_names_request:");
+	console.log(msg);
+	var json_responses = {};
+
+	var username 	= msg.username;
+
+	console.log("LISTENING TO A handle_admin_list_city_names_request WITH msg_payload AS: ");
+	console.log(msg);
+	
+	mongo.connect(mongoURL, function() {
+		console.log('CONNECTED TO MONGO IN handle_admin_list_city_names_request');
+		var collection_login = mongo.collection('login');
+		var json_response= {};
+		
+
+		collection_login.distinct("city", 
+				{ "city" : { $nin : ["", null] }
+		} ,
+			function(err, user) {
+			if (user) {
+				json_responses = {
+					"users" : user
+				};
+				callback(null, json_responses);
+
+			} else {
+				console.log("returned false");
+				json_responses = {
+					"statusCode" : 401
+				};
+				callback(null, json_responses);
+			}
+		});		
+
+
+	});
+
+};
+
+exports.handle_admin_list_city_hosts_request = function (msg, callback) {
+
+	console.log("IN handle_admin_list_city_hosts_request:");
+	console.log(msg);
+	
+	var username 	= msg.username;
+	var city 		= msg.city;
+
+	console.log("LISTENING TO A handle_admin_list_city_hosts_request WITH msg_payload AS: ");
+	console.log(msg);
+	
+	mongo.connect(mongoURL, function() {
+		console.log('CONNECTED TO MONGO IN handle_admin_list_city_hosts_request');
+		var collection_login = mongo.collection('login');
+		var json_response= {};
+		
+		collection_login.find({
+			city : {
+				$eq : city
+			}
+		}).toArray(function(err, items) {
+
+			json_response = {
+				"users" : items
+			};
+			console.log(json_response);
+			callback(null, json_response);
+		});	
+
+	});
+
+};
+
 exports.handle_admin_approve_user_queue_request = function (msg, callback) {
 
 	console.log("IN handle_admin_approve_user_queue_request:");
@@ -156,6 +231,60 @@ exports.handle_admin_approve_user_queue_request = function (msg, callback) {
 				};
 				callback(null, json_responses);
 			}
+		});
+
+	});
+
+};
+
+exports.handle_admin_top_ten_properties_request = function (msg, callback) {
+
+	console.log("IN handle_admin_top_ten_properties_request:");
+	console.log(msg);
+	
+	console.log("LISTENING TO A handle_admin_top_ten_properties_request WITH msg_payload AS: ");
+	
+	mongo.connect(mongoURL, function() {
+		console.log('CONNECTED TO MONGO COLLECTION property IN handle_admin_approve_user_queue_request');
+		var collection_properties = mongo.collection('properties');
+		var json_response= {};
+		
+		collection_properties.find().sort( { revenue: 1 } ).limit( 5 )
+		
+		.toArray(function(err, items) {
+
+			json_response = {
+				"users" : items
+			};
+			console.log(json_response);
+			callback(null, json_response);
+		});
+
+	});
+
+};
+
+exports.handle_admin_citywise_revenue_request = function (msg, callback) {
+
+	console.log("IN handle_admin_citywise_revenue_request:");
+	console.log(msg);
+	
+	console.log("LISTENING TO A handle_admin_citywise_revenue_request WITH msg_payload AS: ");
+	
+	mongo.connect(mongoURL, function() {
+		console.log('CONNECTED TO MONGO COLLECTION property IN handle_admin_approve_user_queue_request');
+		var collection_properties = mongo.collection('properties');
+		var json_response= {};
+		
+		collection_properties.find().sort( { revenue: 1 } ).limit( 5 )
+		
+		.toArray(function(err, items) {
+
+			json_response = {
+				"users" : items
+			};
+			console.log(json_response);
+			callback(null, json_response);
 		});
 
 	});
